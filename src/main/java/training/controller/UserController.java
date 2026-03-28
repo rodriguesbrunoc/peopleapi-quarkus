2467ad302ae06f05b62f71fd11aa6cd692185f2e
unknown <rodriguesbrunoc>
2026-03-27 22:02:25 -0300

    iniciando

---
+++ src/main/java/training/controller/UserController.java
@@ -0,0 +1,13 @@
+package training.controller;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/users")
+public class UserController {
+
+    @GET
+    public String hello() {
+        return "Hello from UserController!";
+    }
+}
